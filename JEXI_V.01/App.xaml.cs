a59162bd943f832a95a58db36b550e2582009fc6
sai ganesh <47131861+Nani-7989@users.noreply.github.com>
2022-04-06 09:07:29 +0530

    Add project files.

---
+++ JEXI_V.01/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+
+namespace JEXI_V._01
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
---
+++ JEXI_V.01/MainWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace JEXI_V._01
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void Grid_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                DragMove();
+            }
+        }
+    }
+}
